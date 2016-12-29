d8f085ffd40e1f6fe355bfb25fb54293f9da8c5b
ravipiyush <piyush.gupta111019@gmail.com>
2016-12-29 15:36:15 +0530

    fortpoints advance

---
+++ app/src/main/java/sodevan/com/forticon/FortpointObject.java
@@ -0,0 +1,39 @@
+package sodevan.com.forticon;
+
+/**
+ * Created by ravipiyush on 29/12/16.
+ */
+
+public class FortpointObject {
+
+    String name ;
+    String text ;
+    String namespace  ;
+    String instance ;
+    String photolink ;
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getNamespace() {
+        return namespace;
+    }
+
+    public String getInstance() {
+        return instance;
+    }
+
+    public String getPhotolink() {
+        return photolink;
+    }
+
+
+
+}
